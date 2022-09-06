d24554c92431d6ced37140acc86de62c4197a22a
alibizhangy <alibizhangy@gmail.com>
2022-09-06 07:23:38 -0700

    add one line our put

--- sample.cpp
+++ sample.cpp
@@ -4,7 +4,8 @@ using namespace std ;
 int main () 
 { 
     cout << "first out put after creatin file " << enld ;
-
+    cout << "adding oneline for testign "; 
+    cout << endl; 
     return 0 ; 
     
 }
