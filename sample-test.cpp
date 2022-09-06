f21b892a6cb8eaa177f9f94d3cfdb2e06e101286
alibizhangy <alibizhangy@gmail.com>
2022-09-06 07:14:03 -0700

    add loop

--- sample-test.cpp
+++ sample-test.cpp
@@ -5,6 +5,11 @@ int main ()
 {
     cout << "hi ali i see you again ! " << endl ;
 
+    for (int i=1 ; i<=5 ; i++) 
+    {
+        cout << "loop = " << i ; 
+        cout << endl ; 
+    }
 
 
     return 0 ; 
